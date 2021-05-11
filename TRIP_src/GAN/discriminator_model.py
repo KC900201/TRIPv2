2cc9f308ade6c0647f01cc7f90fbbeed5b2d6305
kc900201 <nkc900201@gmail.com>
2021-05-11 23:10:37 +0900

    Developing the skeleton template for discriminator_model.py and generator_model.py. Basic template completed for YOLOv3 object detector class files.

--- TRIP_src/GAN/discriminator_model.py
+++ TRIP_src/GAN/discriminator_model.py
@@ -1,2 +1,12 @@
 import torch
 import torch.nn as nn
+
+
+# Initialize discriminator class (basic)
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.disc = nn.Sequential()
+
+    def forward(self, x):
+        return self.disc(x)
--- TRIP_src/GAN/generator_model.py
+++ TRIP_src/GAN/generator_model.py
@@ -1,2 +1,13 @@
 import torch
 import torch.nn as nn
+
+# Initialize generator
+class Generator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.gen = nn.Sequential(
+
+        )
+
+    def forward(self, x):
+        return self.gen(x)
