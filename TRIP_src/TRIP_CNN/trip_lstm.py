ccbc50ba200982a76074a37ead0e950382c18373
kc900201 <nkc900201@gmail.com>
2021-05-19 23:09:40 +0900

    Start modifying codes using Pytorch

--- TRIP_src/TRIP_CNN/trip_lstm.py
+++ TRIP_src/TRIP_CNN/trip_lstm.py
@@ -6,29 +6,39 @@
 Date          Comment
 ========================
 05152021      First revision
+05192021      Recoding using Pytorch library
 """
 
 import torch
 import cupy
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 
 
-class TripLSTM():
-    """A class of TRIP(Traffic Risk Prediction) model
+class TripLSTM(nn.Module):
+    """
+        A class of TRIP(Traffic Risk Prediction) model
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, input_size, hidden_size):
+        super(TripLSTM, self).__init__()
+        with self.init_scope():
+            self.input = nn.Linear(in_features=None, out_features=input_size)
+            self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size)
+            self.lstm2 = nn.LSTM(input_size=hidden_size, hidden_size=hidden_size)
+            self.lstm3 = nn.LSTM(input_size=hidden_size, hidden_size=hidden_size)
+            self.ho = nn.Linear(in_features=hidden_size, out_features=1)
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, x):
         """ Forward propagation
             Args:
              x (a Variable of feature array): a feature array
             Returns:
              h (a Variable of hidden state array): a hidden state array
         """
-        pass
+        z = F.tanh(self.input(x))
+        return self.lstm(z)
 
     def predict_risk(self, x):
         """ Risk prediction
@@ -37,7 +47,13 @@ class TripLSTM():
             Returns:
              r (a Variable of float): a risk value
         """
-        pass
+        # reset lstm state
+        self.lstm.reset_parameters()
+        # recurrent reasoning and risk prediction
+        for t in range(len(x)):
+            v = x # Need to modify this, chainer => Variable, how does pytorch replace this?
+            h = self(v)
+        return F.sigmoid(self.ho(h))
 
     def predict_mean_risk(self, x):
         """ Risk prediction (mean)
